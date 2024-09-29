7354e0524aae335e8eacf702d6c2f7e542e0d607
youssef <youssefhsn16@gmail.com>
2024-09-29 11:56:38 +0300

    🏗️ Create project using XCode

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,20 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Youssef Al-Hassan on 29/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ Little Lemon/Little LemonTests/Little_LemonTests.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonTests.swift
+//  Little LemonTests
+//
+//  Created by Youssef Al-Hassan on 29/09/2024.
+//
+
+import Testing
+@testable import Little_Lemon
+
+struct Little_LemonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
